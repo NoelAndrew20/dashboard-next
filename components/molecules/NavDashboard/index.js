4cbe41be01c683847b45f449c54913003815cf1e
NoelAndrew <andrew98noel@gmail.com>
2023-08-04 15:12:59 -0600

    feature: se hizo el dashboard de agregar usuarios

--- components/molecules/NavDashboard/index.js
+++ components/molecules/NavDashboard/index.js
@@ -2,19 +2,7 @@ const NavDashboard = ({ svg, section, id }) => {
     return (
         <div className="dashboard-nav" id={ id }>
             <div className="dashboard-section">
-                <div className="cerdito">
-                    <img src={ svg } />                          
-                </div>
                 <h1>Dashboard: { section }</h1>
-            </div>
-            <div className="dashboard-filtro">
-                <label>Filtrar por zona:</label>
-                <select>
-                    <option value="volvo">Santiago</option>
-                    <option value="saab">Opcion 1</option>
-                    <option value="mercedes">Opcion 2</option>
-                    <option value="audi">Opcion 3</option>
-                </select>
             </div>
         </div>
     )
