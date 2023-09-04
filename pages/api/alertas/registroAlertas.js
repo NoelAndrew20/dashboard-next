dcdedfd1d7d023766b6301c6fd0e283c632eda73
Jesus-termo <proyectoConstanza06@outlook.com>
2023-09-04 09:55:18 -0600

    fix date alerts monterrey

--- pages/api/alertas/registroAlertas.js
+++ pages/api/alertas/registroAlertas.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); // Import the cors module
 const moment = require('moment-timezone');
-const tz = 'America/Havana';
+const tz = 'America/Monterrey';
 
 const app = express();
 app.use(cors());
