b3244283e00e37ebea3f3f254b399bc0902fb7ba
Jesus-termo <proyectoConstanza06@outlook.com>
2023-09-04 10:07:21 -0600

    fix date in code Eugenio

--- pages/api/alertas/registroAlertas.js
+++ pages/api/alertas/registroAlertas.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); // Import the cors module
-const moment = require('moment-timezone');
-const tz = 'America/Merida';
+//const moment = require('moment-timezone');
+//const tz = 'America/Merida';
 
 const app = express();
 app.use(cors());
@@ -29,9 +29,9 @@ app.get("/getAllAlerta", async (req, res) => {
     .sort({ fecha: -1 })
     .limit(30);
 
-    allAlerta.forEach((alerta) => {
-      alerta.fecha = moment(alerta.fecha).tz(tz).format('YYYY-MM-DD HH:mm:ss');
-    });
+    //allAlerta.forEach((alerta) => {
+    //  alerta.fecha = moment(alerta.fecha).tz(tz).format('YYYY-MM-DD HH:mm:ss');
+    //});
 
     res.send({ status: "ok", data: allAlerta });
   } catch (error) {
