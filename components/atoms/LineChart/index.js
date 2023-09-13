2715a3c0a15182a3dbbab99d44bf551399a10a9f
NoelAndrew <andrew98noel@gmail.com>
2023-09-13 09:53:18 -0600

    feature: se creó la pagina del graficador

---
+++ components/atoms/LineChart/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const LineChart = ({ dataArray }) => {
+
+console.log("dataarray",dataArray)
+    const data = {
+        labels: dataArray.map(item => item.dia),
+        datasets: [
+        {
+            label: 'Dataset 1',
+            data: dataArray.map(item => parseInt(item.kg)),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        ],
+    };
+    
+    return <Line data={data} />;
+}
+export default LineChart;
+
