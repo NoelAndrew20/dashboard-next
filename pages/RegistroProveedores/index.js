a40e9c9ff0d8432bc9743a9f4d374f952ee2dbc3
NoelAndrew <andrew98noel@gmail.com>
2023-10-11 15:10:27 -0600

    feature: page de proveedores

---
+++ pages/RegistroProveedores/index.js
@@ -0,0 +1,43 @@
+import NavDashboard from '@/components/molecules/NavDashboard';
+import Navigation from '@/components/molecules/Navigation';
+import StaticMeta from '@/components/atoms/StaticMeta';
+import ProveedorForm from '@/components/molecules/ProveedorForm';
+import { useState, useEffect } from 'react';
+import { useDarkMode } from '@/context/DarkModeContext';
+const RegistroProveedores = ({ title, description, image }) => {
+    const { isDarkMode, toggleDarkMode } = useDarkMode();
+
+    return(
+        <div className={`${isDarkMode ? "darkMode" : "lightMode" } full-viewport`}>
+        <StaticMeta
+            title={title}
+            description={description}
+            image={image}
+        />     
+        <div>
+            <Navigation/>
+            <NavDashboard section="Proveedores"/>
+        </div>
+        <div className="wrapper">
+          <div className="mt-3">
+            <ProveedorForm/>
+          </div>
+        </div>
+    </div>
+    )
+}
+export default RegistroProveedores;
+
+export const getServerSideProps = async () => {
+const title = "Constanza";
+const description =
+  "Dashboard de proveedores";
+const image = "images/icon/logo-400.png";
+return {
+  props: {
+    title,
+    description,
+    image,
+  },
+};
+};
