f2f4fc1328fbb542f67d7d555f1bdc04545147d0
NoelAndrew <andrew98noel@gmail.com>
2023-10-16 12:43:52 -0600

    feature: se hicieron más cambios en la lógica del dashboard de menú

--- components/atoms/CalcuForm/index.js
+++ components/atoms/CalcuForm/index.js
@@ -1,11 +1,12 @@
 import { useDarkMode } from "@/context/DarkModeContext";
 import { useEffect, useState } from "react";
-const CalcuForm = ({addFormData, alimento, selectedFoodData,}) => {
+const CalcuForm = ({addFormData, alimento, selectedFoodData}) => {
     const { isDarkMode, toggleDarkMode } = useDarkMode();
     const entriesPerPage = 10;
     const [currentPage, setCurrentPage] = useState(1);
     const startIndex = (currentPage - 1) * entriesPerPage;
     const endIndex = startIndex + entriesPerPage;
+    
     const [formData, setFormData] = useState(selectedFoodData || {
         nombreAlimento: '',
         cantidad: '',
@@ -15,10 +16,9 @@ const CalcuForm = ({addFormData, alimento, selectedFoodData,}) => {
       const nuevoLote = {
         nombreAlimento: formData.nombreAlimento,
         cantidad: formData.cantidad,
-      
+        
       };
-      addFormData(nuevoLote);
-  
+      addFormData(nuevoLote);  
       // Resto de la lógica
     };
     
@@ -28,84 +28,82 @@ const CalcuForm = ({addFormData, alimento, selectedFoodData,}) => {
 
     return(
         <div>
-        <h2 className="text-lg">Generar calculo: <span className="text-[#D4AF37]">{alimento}</span></h2>
-        <div>    
-          {selectedFoodData && (
+          <div>    
+            {selectedFoodData && (
 
-            <div className="flex">
-                                    
+              <div className="flex">
+                                      
 
-              <div className="modal-item w-1/2">
-                <p>Nombre de alimento:</p>
-                <input
-                  className={isDarkMode ? "edit-input-container-d" : "edit-input-container"}
-                  type="text"
-                  name="NombreAlimento"
-                  value={formData.nombreAlimento}
-                  onChange={(e) => setFormData({ ...formData, nombreAlimento: e.target.value })}
-                  required
-                />
-              </div>
-              <div className="modal-item w-1/2">
-                <p>Cantidad:</p>
-                <input
+                <div className="modal-item w-1/2">
+                  <p className="font-bold">Nombre de alimento:</p>
+                  <input
+                    className={isDarkMode ? "edit-input-container-d" : "edit-input-container"}
+                    type="text"
+                    name="NombreAlimento"
+                    value={formData.nombreAlimento}
+                    onChange={(e) => setFormData({ ...formData, nombreAlimento: e.target.value })}
+                    required
+                  />
+                </div>
+                <div className="modal-item w-1/2">
+                  <p className="font-bold">Cantidad:</p>
+                  <input
                     className={isDarkMode ? "edit-input-container-d" : "edit-input-container"}
                     type="number"
                     name="cantidad"
                     value={formData.cantidad}
                     onChange={(e) => setFormData({ ...formData, cantidad: e.target.value })}
+                  />
+                </div>
+                {/*
+                <div className="modal-item w-1/3">
+                  <p>% de Proteina:</p>
+                  <input
+                    className={isDarkMode ? "edit-input-container-d" : "edit-input-container"}
+                    type="number"
+                    name="proteina"
+                    value={formData.proteina}
+                    onChange={(e) => setFormData({ ...formData, proteina: e.target.value })}
                     required
-                />
+                  />
+            </div>*/}
               </div>
-              {/*
-              <div className="modal-item w-1/3">
-                <p>% de Proteina:</p>
-                <input
-                  className={isDarkMode ? "edit-input-container-d" : "edit-input-container"}
-                  type="number"
-                  name="proteina"
-                  value={formData.proteina}
-                  onChange={(e) => setFormData({ ...formData, proteina: e.target.value })}
-                  required
-                />
-          </div>*/}
+            )}
+            {/* Otros campos */}
+          </div>
+          {/*
+          <div className="flex">
+            <div className="modal-item w-1/3">
+              <p>Precio:</p>
+              <input
+                className={isDarkMode ? "edit-input-container-d" : "edit-input-container"}
+                type="number"
+                name="precio"
+                value={formData.precio}
+                onChange={(e) => setFormData({ ...formData, precio: e.target.value })}
+                required
+              />
             </div>
-          )}
-          {/* Otros campos */}
-        </div>
-        {/*
-        <div className="flex">
-          <div className="modal-item w-1/3">
-            <p>Precio:</p>
-            <input
-              className={isDarkMode ? "edit-input-container-d" : "edit-input-container"}
-              type="number"
-              name="precio"
-              value={formData.precio}
-              onChange={(e) => setFormData({ ...formData, precio: e.target.value })}
-              required
-            />
+            <div className="modal-item w-1/3">
+              <p>Precio variable:</p>
+              <input
+                className={isDarkMode ? "edit-input-container-d" : "edit-input-container"}
+                type="number"
+                name="precioVariable"
+                value={formData.precioVariable}
+                onChange={(e) => setFormData({ ...formData, precioVariable: e.target.value })}
+                required
+              />
+            </div>
+          </div> 
+          <div className="flex pt-5 justify-center text-lg bold">
+            <h2>Total de proteina: {isNaN(totalProte) ? 0 : totalProte}</h2>
           </div>
-          <div className="modal-item w-1/3">
-            <p>Precio variable:</p>
-            <input
-              className={isDarkMode ? "edit-input-container-d" : "edit-input-container"}
-              type="number"
-              name="precioVariable"
-              value={formData.precioVariable}
-              onChange={(e) => setFormData({ ...formData, precioVariable: e.target.value })}
-              required
-            />
+          <div className="flex pt-5 justify-center text-lg bold">
+            <h2>Total de precio: {isNaN(totalPrecio) ? 0 : totalPrecio}</h2>
           </div>
-        </div> 
-        <div className="flex pt-5 justify-center text-lg bold">
-          <h2>Total de proteina: {isNaN(totalProte) ? 0 : totalProte}</h2>
+          <button  className="button" onClick={agregarLote}>Agregar al total</button>*/}
         </div>
-        <div className="flex pt-5 justify-center text-lg bold">
-          <h2>Total de precio: {isNaN(totalPrecio) ? 0 : totalPrecio}</h2>
-        </div>
-        <button  className="button" onClick={agregarLote}>Agregar al total</button>*/}
-      </div>
     )
 }
 export default CalcuForm;
