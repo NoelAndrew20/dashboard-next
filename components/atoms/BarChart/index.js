98f97141450f43d114edfffe031ae82a08f69a2c
NoelAndrew <andrew98noel@gmail.com>
2023-11-16 15:19:39 -0600

    hotfix: cambio de color a las graficas

--- components/atoms/BarChart/index.js
+++ components/atoms/BarChart/index.js
@@ -31,7 +31,10 @@ const data = {
     {
       label: 'Vientre',
       data: [Math.round(jsonData.alimento.costo_etapa_A.Vientre.C*100)/100,jsonData.alimento.costo_etapa_A.Vientre.A1,Math.round(jsonData.alimento.costo_etapa_A.Vientre.A2*100)/100,jsonData.alimento.costo_etapa_A.Vientre.A3,Math.round(jsonData.alimento.costo_etapa_A.Vientre.A4*100)/100,jsonData.alimento.costo_etapa_A.Vientre.G1,Math.round(jsonData.alimento.costo_etapa_A.Vientre.M1C*100)/100,jsonData.alimento.costo_etapa_A.Vientre.M1D,Math.round(jsonData.alimento.costo_etapa_A.Vientre.Z1*100)/100,jsonData.alimento.costo_etapa_A.Vientre.G2,Math.round(jsonData.alimento.costo_etapa_A.Vientre.M2C*100)/100,jsonData.alimento.costo_etapa_A.Vientre.M2D,Math.round(jsonData.alimento.costo_etapa_A.Vientre.Z2*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.G3*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.M3C*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.M3D*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.Z3*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.G4*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.M4C*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.M4D*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.Z4*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.G5*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.M5C*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.M5D*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.Z5*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.G6*100)/100,Math.round(jsonData.alimento.costo_etapa_A.Vientre.M6C*100)/100,],
-      backgroundColor: 'rgba(233,151,9, 0.6)',
+      backgroundColor: [
+        "rgba(200, 145, 22, 1)",
+        "rgb(13, 61, 100)"
+      ],
     },
   ],
 };
--- components/atoms/BarGestación2/index.js
+++ components/atoms/BarGestación2/index.js
@@ -25,12 +25,8 @@ const BarGestación2 = () => {
         label: 'Total', // Agrega una etiqueta
         data: totals,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.6)',
-          'rgba(54, 162, 235, 0.6)',
-          'rgba(255, 206, 86, 0.6)',
-          'rgba(75, 192, 192, 0.6)',
-          'rgba(153, 102, 255, 0.6)',
-          'rgba(255, 159, 64, 0.6)',
+          "rgba(200, 145, 22, 1)",
+          "rgb(13, 61, 100)"
         ],
       },
     ],
