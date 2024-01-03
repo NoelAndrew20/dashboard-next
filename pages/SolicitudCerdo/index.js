94621a567c1bbfb7e0b4ddd176f5260a7a3dbfe0
NoelAndrew <andrew98noel@gmail.com>
2024-01-03 09:19:07 -0600

    feature:tabla en graficator 2

--- pages/SolicitudCerdo/index.js
+++ pages/SolicitudCerdo/index.js
@@ -8,6 +8,7 @@ import TableSC from '@/components/molecules/TableSC';
 import RazaTable from '@/components/atoms/RazaTable';
 import jwt from 'jsonwebtoken';
 import { useRouter } from 'next/router';
+import TablePig from '@/components/molecules/TablePig';
 
 const axios = require('axios');
 
@@ -18,7 +19,28 @@ const SolicitudCerdo = ({ title, description, image }) => {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
   const [dataOrder, setDataOrder] = useState([])
   const [data, setData] = useState([]);
-  const [data2, setData2] = useState([]);
+  const [dataPig, setDataPig] = useState(
+    [
+      {
+        _id: "65946d858bcf58efd19e7b69",
+        lote: "SWILPAD001",
+        tipo: "Semental",
+        cantidad: 104,
+        fechaLicitacion: "29-01-2030",
+        fechaDia0: "20-11-2029",
+        status: "Requerido"
+      },
+      {
+        _id: "65946d858bcf58efd19e7b6a",
+        lote: "SWILPAE001",
+        tipo: "Semental",
+        cantidad: 104,
+        fechaLicitacion: "26-02-2030",
+        fechaDia0: "18-12-2029",
+        status: "Requerido"
+      }
+    ]
+  );
   const [dataList, setDataList] = useState([]);
   const [dataGraph, setDataGraph] = useState([
     {
@@ -108,56 +130,54 @@ const SolicitudCerdo = ({ title, description, image }) => {
   }, []);
 
   
-    useEffect(() => {
-      const checkToken = async () => {
-        try {
-          const token = localStorage.getItem('token');
-          if (!token) {
-            router.push('/Login');
-            return;
-          }
-    
-          const decodedToken = jwt.decode(token);
-          const usuario = decodedToken.usuario;
-          const nombre = decodedToken.nombre;
-          const proveedor = decodedToken.proveedor;
-          //console.log("Usuario:", usuario);
-          //console.log("Nombre:", nombre);
-          //console.log("Proveedor:", proveedor);
-          setUsername(usuario);
-    
-          setTokenVerified(true);
-        } catch (error) {
-          console.error('Error al verificar el token:', error);
-          setTokenVerified(true);
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          router.push('/Login');
+          return;
         }
-      };
-      checkToken();
-    }, [router]);
-    
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get('http://192.168.100.10:3085/getAllsolicitudCerdo');
-          const jsonData = response.data;
-          setDataGraph(jsonData);
-        } catch (error) {
-          console.error('Error al obtener datos:', error);
-        }
-      };
-    
-      if (tokenVerified) {
-        fetchData();
+  
+        const decodedToken = jwt.decode(token);
+        const usuario = decodedToken.usuario;
+        const nombre = decodedToken.nombre;
+        const proveedor = decodedToken.proveedor;
+        //console.log("Usuario:", usuario);
+        //console.log("Nombre:", nombre);
+        //console.log("Proveedor:", proveedor);
+        setUsername(usuario);
+  
+        setTokenVerified(true);
+      } catch (error) {
+        console.error('Error al verificar el token:', error);
+        setTokenVerified(true);
       }
-    }, [tokenVerified, setUsuario]);  
+    };
+    checkToken();
+  }, [router]);
+    
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://192.168.100.10:3085/getAllsolicitudCerdo');
+        const jsonData = response.data;
+        setDataGraph(jsonData);
+      } catch (error) {
+        console.error('Error al obtener datos:', error);
+      }
+    };
+  
+    if (tokenVerified) {
+      fetchData();
+    }
+  }, [tokenVerified, setUsuario]);  
     
   
   if (!tokenVerified) {
     // Puedes mostrar un indicador de carga aquí si lo deseas
     return null;
   }
-
-  
 
     return (
         <div className={`${isDarkMode ? "darkMode" : "lightMode" } full-viewport`}>
@@ -171,21 +191,25 @@ const SolicitudCerdo = ({ title, description, image }) => {
                 <NavDashboard section="Solicitud de Cerdo" svg={svg}/>
             </div>
             <div className="wrapper">
-                <div className="mt-10">
-                    <h2 className="text-xl mt-5 mb-5">Solicitud de Cerdo</h2>
-                    <TableSC 
-                    dataList={dataList}
-                    setDataList={setDataList}
-                    data={dataGraph} 
-                    setData={setDataGraph} 
-                    dataOrder={dataOrder} 
-                    setDataOrder={setDataOrder}
-                    />
-                </div>
-                <div className="mt-10">
-                    <h2 className="text-xl mt-5 mb-5">Solicitudes existentes</h2>
-                    <RazaTable data={dataList} setData={setDataList}/>
-                </div>
+            <div className="mt-10">
+                <h2 className="text-xl mt-5 mb-5">Registro de cerdos</h2>
+                <TablePig data={ dataPig } />
+              </div>
+              <div className="mt-10">
+                  <h2 className="text-xl mt-5 mb-5">Solicitud de Cerdo</h2>
+                  <TableSC 
+                  dataList={dataList}
+                  setDataList={setDataList}
+                  data={dataGraph} 
+                  setData={setDataGraph} 
+                  dataOrder={dataOrder} 
+                  setDataOrder={setDataOrder}
+                  />
+              </div>
+              <div className="mt-10">
+                  <h2 className="text-xl mt-5 mb-5">Solicitudes existentes</h2>
+                  <RazaTable data={dataList} setData={setDataList}/>
+              </div>
             </div>
         </div>
     )
